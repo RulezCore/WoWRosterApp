a9d0b5bb474db8a00146a81f1e9d15b1fabc3abd
Héctor Costa Guzmán <hcostaguzman@gmail.com>
2019-10-04 18:59:34 +0200

    pusheando el frontend

--- api/views.py
+++ api/views.py
@@ -3,6 +3,8 @@ from .serializers import RaidSerializer
 from rest_framework import viewsets
 
 # Create your views here.
+
+
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Raid.objects.all()
     serializer_class = RaidSerializer
