043fa74403619a09c576011465f26e2de23d5253
Héctor Costa Guzmán <hcostaguzman@gmail.com>
2019-10-07 19:12:56 +0200

    toflama

--- api/models.py
+++ api/models.py
@@ -1,13 +1,17 @@
+from django.db.models.signals import post_save
 from django.db import models
 
 # Create your models here.
+
+
 class Raid(models.Model):
     NAMES = (
         ('onyxia', 'onyxia'),
         ('mc', 'molten core')
     )
 
-    name = models.CharField(choices=NAMES, verbose_name="Nombre", max_length=150)
+    name = models.CharField(
+        choices=NAMES, verbose_name="Nombre", max_length=150)
     day = models.DateField(verbose_name="Dia del evento")
     notes = models.TextField(blank=True, null=True, verbose_name="Notas")
 
@@ -16,10 +20,8 @@ class Raid(models.Model):
         verbose_name_plural = "Raids"
         ordering = ['-day']
 
-
     def __str__(self):
-       return self.name
-
+        return self.name
 
 
 class Member(models.Model):
@@ -48,11 +50,14 @@ class Member(models.Model):
         ('priest', 'Sacerdote')
     )
 
-    name = models.CharField(max_length=90, verbose_name="Nickname", unique=True)
+    name = models.CharField(
+        max_length=90, verbose_name="Nickname", unique=True)
     rank = models.CharField(choices=TYPE, verbose_name="Rango", max_length=150)
     role = models.CharField(choices=ROLES, verbose_name="Rol", max_length=150)
-    clas = models.CharField(choices=CLASES, verbose_name="Clase", max_length=150)
-    joined = models.DateField(blank=True, null=True, verbose_name="Fecha de entrada")
+    clas = models.CharField(
+        choices=CLASES, verbose_name="Clase", max_length=150)
+    joined = models.DateField(blank=True, null=True,
+                              verbose_name="Fecha de entrada")
     notes = models.TextField(blank=True, null=True, verbose_name="Notas")
 
     class Meta:
@@ -65,8 +70,10 @@ class Member(models.Model):
 
 
 class RaidAssistance(models.Model):
-    member = models.ForeignKey(Member, verbose_name="Miembro", on_delete=models.CASCADE)
-    raid = models.ForeignKey(Raid, verbose_name="Raid", on_delete=models.CASCADE, related_name='raid_assistances')
+    member = models.ForeignKey(
+        Member, verbose_name="Miembro", on_delete=models.CASCADE)
+    raid = models.ForeignKey(Raid, verbose_name="Raid",
+                             on_delete=models.CASCADE, related_name='raid_assistances')
     assistance = models.BooleanField(default=False, verbose_name="Asistencia")
 
     class Meta:
@@ -89,7 +96,5 @@ def create_raid_assistance(sender, instance, created, **kwargs):
             raid_assistance.raid = instance
             raid_assistance.save()
 
-
-from django.db.models.signals import post_save
 
 post_save.connect(create_raid_assistance, sender=Raid)
